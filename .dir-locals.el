c6565379b05124e0fc0a2c2d3be2fde553b7a0b4
Miko Nieminen <miko.nieminen@iki.fi>
2022-04-20 12:58:19 +0200

    Prepare repo for multiple contracts
    Turn the repo into Cargo workspace with multiple crates to prepare it
    for additional contracts.
    
    Also rename `rust` folder as `token` to reflect the purpose of the
    code under this crate.

--- .dir-locals.el
+++
@@ -1,5 +0,0 @@
-((nil . ()) ;; no settings for all modes
- (rustic-mode ;; for rustic mode
-  ;; ensure the buffer workspace is always set to the project root to work around
-  ;; brotzeit/rustic#179 and brotzeit/rustic#236
-  . ((eval . (advice-add 'rustic-buffer-workspace :override (lambda (&optional ignored) (format "%srust/" (projectile-project-root))))))))
