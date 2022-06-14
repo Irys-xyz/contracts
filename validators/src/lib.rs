837140c91c0945f22fa766e280411087fb6d414e
Miko Nieminen <miko.nieminen@iki.fi>
2022-06-14 13:36:04 +0200

    Export more symbols from validators contract

--- validators/src/lib.rs
+++ validators/src/lib.rs
@@ -5,3 +5,8 @@ pub mod contract_utils;
 mod epoch;
 mod error;
 mod state;
+
+pub use actions::slashing;
+pub use bundlr_contracts_shared::{Address, Amount};
+pub use epoch::Epoch;
+pub use state::{State, Validator};
